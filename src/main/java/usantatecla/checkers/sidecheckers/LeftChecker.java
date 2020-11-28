18a7948a2c1ecdaf07f40e452faf2494eb81697c
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-11-28 17:26:47 +0100

    Create general checkers

---
+++ src/main/java/usantatecla/checkers/sidecheckers/LeftChecker.java
@@ -0,0 +1,25 @@
+package usantatecla.checkers.sidecheckers;
+
+import usantatecla.Interval;
+import usantatecla.checkers.IntersectionChecker;
+import usantatecla.checkers.leftcheckers.CompareIntervalIncludeIntervalChecker;
+import usantatecla.checkers.leftcheckers.LeftIntersectionChecker;
+import usantatecla.checkers.leftcheckers.PointIntersectionChecker;
+
+public class LeftChecker extends IntersectionChecker {
+
+    private final IntersectionChecker pointIntersectionChecker = new PointIntersectionChecker(null);
+    private final IntersectionChecker compareIntervalIncludeIntervalChecker = new CompareIntervalIncludeIntervalChecker(pointIntersectionChecker);
+    private final IntersectionChecker leftIntersectionChecker = new LeftIntersectionChecker(compareIntervalIncludeIntervalChecker);
+
+    public LeftChecker(IntersectionChecker nextChecker) {
+        super(nextChecker);
+    }
+
+    public boolean handle(Interval interval, Interval compareInterval) {
+        if (compareInterval.min.value < interval.min.value) {
+            return leftIntersectionChecker.handle(interval, compareInterval);
+        }
+        return this.setNext(interval, compareInterval);
+    }
+}
---
+++ src/main/java/usantatecla/checkers/sidecheckers/MiddleChecker.java
@@ -0,0 +1,21 @@
+package usantatecla.checkers.sidecheckers;
+
+import usantatecla.Interval;
+import usantatecla.checkers.IntersectionChecker;
+
+public class MiddleChecker extends IntersectionChecker {
+    public MiddleChecker(IntersectionChecker nextChecker) {
+        super(nextChecker);
+    }
+
+    public boolean handle(Interval interval, Interval compareInterval) {
+        if (interval.max.isWithin(interval.max.value) && interval.min.isWithin(interval.min.value)
+                && compareInterval.max.isWithin(compareInterval.max.value)
+                && compareInterval.max.isWithin(compareInterval.max.value)
+                && Double.compare(compareInterval.max.value, interval.max.value) == 0
+                && Double.compare(compareInterval.min.value, interval.min.value) == 0) {
+            return true;
+        }
+        return this.setNext(interval, compareInterval);
+    }
+}
