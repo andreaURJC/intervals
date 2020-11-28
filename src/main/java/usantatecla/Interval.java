208a7cd674a2dcfb94dd977ef2e888a6935e7984
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-11-28 11:49:24 +0100

    test passed

--- src/main/java/usantatecla/Interval.java
+++ src/main/java/usantatecla/Interval.java
@@ -2,67 +2,70 @@ package usantatecla;
 
 public class Interval {
 
-	public Min min;
-	public Max max;
+    public Min min;
+    public Max max;
 
-	public Interval(Min min, Max max) {
-		assert min.value <= max.value;
-		this.min = min;
-		this.max = max;
-	}
+    public Interval(Min min, Max max) {
+        assert min.value <= max.value;
+        this.min = min;
+        this.max = max;
+    }
 
-	public boolean include(double value) {
-			return this.min.isWithin(value) && this.max.isWithin(value);
-	}
+    public boolean include(double value) {
+        return this.min.isWithin(value) && this.max.isWithin(value);
+    }
 
-	public boolean isIntersected(Interval comparedInterval) {
-		if(comparedInterval.min.value < this.min.value  && comparedInterval.max.value < this.max.value ) {
-			return true;
-		}
-		if(comparedInterval.min.value > this.min.value  && comparedInterval.max.value > this.max.value ) {
-			return true;
-		}
-		if(comparedInterval.min.value > this.min.value && comparedInterval.max.value < this.max.value) {
-			return true;
-		}
-		return false;
-	}
+    public boolean isIntersected(Interval comparedInterval) {
+        if (comparedInterval.min.value < this.min.value && comparedInterval.max.value < this.max.value) {
+            return true;
+        }
+        if (comparedInterval.min.value > this.min.value && comparedInterval.max.value > this.max.value) {
+            return true;
+        }
+        if (comparedInterval.min.value > this.min.value && comparedInterval.max.value < this.max.value) {
+            return true;
+        }
+        if (comparedInterval.min.value < this.min.value && comparedInterval.max.value > this.max.value) {
+            return true;
+        }
+        return false;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((max == null) ? 0 : max.hashCode());
-		result = prime * result + ((min == null) ? 0 : min.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((max == null) ? 0 : max.hashCode());
+        result = prime * result + ((min == null) ? 0 : min.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Interval other = (Interval) obj;
-		if (max == null) {
-			if (other.max != null)
-				return false;
-		} else if (!max.equals(other.max))
-			return false;
-		if (min == null) {
-			if (other.min != null)
-				return false;
-		} else if (!min.equals(other.min))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Interval other = (Interval) obj;
+        if (max == null) {
+            if (other.max != null)
+                return false;
+        } else if (!max.equals(other.max))
+            return false;
+        if (min == null) {
+            if (other.min != null)
+                return false;
+        } else if (!min.equals(other.min))
+            return false;
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return this.min.toString() + ", " + max.toString();
-	}
+    @Override
+    public String toString() {
+        return this.min.toString() + ", " + max.toString();
+    }
 
 
 }
