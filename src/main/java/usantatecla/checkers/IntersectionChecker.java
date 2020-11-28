e8afd8ba8dde3f9d5e457357c60da685ab1e106d
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-11-28 14:31:52 +0100

    Added IntersectionChecker abstract class

---
+++ src/main/java/usantatecla/checkers/IntersectionChecker.java
@@ -0,0 +1,20 @@
+package usantatecla.checkers;
+
+import usantatecla.Interval;
+
+public abstract class IntersectionChecker {
+    protected IntersectionChecker nextChecker;
+
+    public IntersectionChecker(IntersectionChecker nextChecker) {
+        this.nextChecker = nextChecker;
+    }
+
+    public boolean setNext(Interval interval, Interval compareInterval) {
+        if (nextChecker == null) {
+            return true;
+        }
+        return nextChecker.handle(interval, compareInterval);
+    }
+
+    public abstract boolean handle(Interval interval, Interval compareInterval);
+}
